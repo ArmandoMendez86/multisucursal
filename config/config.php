bb0fc42c200b1123280c68f862cf21fd0ff74c84
ArmandoMendez86 <desarrolloweb150305@gmail.com>
2025-08-29 15:25:13 -0600

    Arreglando agregar producto2

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+// Archivo: /config/config.php
+
+// =========== CREDENCIALES BASE DE DATOS PRINCIPAL ===========
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'Linux861215');
+define('DB_NAME', 'update_mega');
+define('DB_CHARSET', 'utf8mb4');
+
+
+// =========== CREDENCIALES BASE DE DATOS SECUNDARIA (si la necesitas) ===========
+define('DB2_HOST', 'localhost');
+define('DB2_USER', 'megapart_update');
+define('DB2_PASSWORD', 'Update861215#-');
+define('DB2_NAME', 'megapart_update'); // Cambia esto si es otra DB
+
+
+// =========== CONFIGURACIÓN GENERAL DE LA APP ===========
+define('URL', '/multi-sucursal/');
+
+?>
